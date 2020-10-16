de2ec6c41760addd6ea62f51df3c7cccdee4f054
jelly octane <jamie.de.lange@mac.com>
2020-10-15 21:47:30 -0500

    fight optimization

--- assets/js/game.js
+++ assets/js/game.js
@@ -84,6 +84,13 @@ var fightOrSkip = function() {
 };
 
 var fight = function(enemy) {
+
+  var isPlayerTurn = true; 
+
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
+
   while (playerInfo.health > 0 && enemy.health > 0) {
     if (fightOrSkip()) {
       break;
@@ -128,6 +135,7 @@ var fight = function(enemy) {
     } else {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
+    isPlayerTurn = !isPlayerTurn;
   }
 };
 
